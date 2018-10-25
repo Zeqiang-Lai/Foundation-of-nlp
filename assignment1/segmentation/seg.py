456f5ff6ecdeb2f33ea3fb86a4cd33d4937aeb0e
Zeqiang-Lai <916323301@qq.com>
2018-10-25 20:19:39 +0800

    add hmm seg test

--- assignment1/segmentation/seg.py
+++ assignment1/segmentation/seg.py
@@ -1,15 +1,23 @@
+import timeit
+import os
+
 from model import dict_generator 
 from model.mm_seg import MMSeg
 from model.unigram_seg import UniGramSeg
-import timeit
+from model.hmm_seg import HmmSeg
 
 if __name__ == '__main__':
 
     # seg = MMSeg()
-    seg = UniGramSeg()
+    # seg = UniGramSeg()
     # mdict = dict_generator.load_sogou_dict('datasets/SogouLabDic.dic')
-    mdict = dict_generator.json_read("model/dicts/pku_dict.json", encoding='utf-16')
-    seg.set_dict(mdict)
+    # mdict = dict_generator.json_read("model/dicts/pku_dict.json", encoding='utf-16')
+    # seg.set_dict(mdict)
+
+    script_dir = os.path.dirname(__file__)
+    seg = HmmSeg()
+    model_path = os.path.join(script_dir, "model/hmm_para")
+    seg.load(model_path)
 
     pred = []
     ch_count = 0
@@ -18,21 +26,31 @@ if __name__ == '__main__':
     with open(test_path, "r", encoding='utf-8') as f:
         lines = f.readlines()
         start = timeit.default_timer()
+        count = 0
+        total_count = len(lines)
         for sent in lines:
-            pred.append(seg.cut(sent))
-            ch_count += len(sent)
+            try:
+                pred.append(seg.cut(sent))
+                ch_count += len(sent)
+            except:
+                pred.append(["Error"] * len(sent))
+                continue
+            count += 1
+            if count % 500 == 0:
+                print("Processed {0}/{1} ---- {2}".format(count, total_count, count/total_count))
         end = timeit.default_timer()
         time_cost = end - start
+
     
     print("Total number of characters: {0}.".format(ch_count))
     print("Time cost: {0}s.".format(time_cost))
     print("Processed characters per second: {0}.".format(int(ch_count / time_cost)) )
 
     # 保存分词结果
-    save_path = "pku_seg/pku_uni_seg.txt"
+    save_path = "pku_seg/pku_hmm_seg.txt"
     with open(save_path, "w", encoding='gb18030') as f:
         for words in pred:
-            s = " ".join(words)
+            s = " ".join(words) + "\n"
             s.encode('gb18030')
             f.write(s)
 
